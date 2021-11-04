5417c4b6945fa37bc8523e124846ddd323e520bd
SteHun <stefanhuneker@hotmail.com>
2021-11-04 14:54:05 +0100

    cleaned up code and changed default activate key to shift

--- interval_monitor.py
+++ interval_monitor.py
@@ -2,44 +2,46 @@
 import keyboard
 import time
 
-timestamps = []
-keyboard.wait("space")
+ACTIVATE_KEY = "shift"
+STOP_KEY = "alt"
 
-def add_current_time(timestamps):
+def main():
+    timestamps = []
+    keyboard.wait(ACTIVATE_KEY)
+    add_current_time(timestamps)
+    keyboard.add_hotkey(ACTIVATE_KEY , add_current_time, args=(timestamps,))
+    keyboard.wait(STOP_KEY)
+
+def add_current_time(timestamps, write = True):
     timestamps.append(time.time())
-    print(f"{calculate_last_bpm(timestamps)}, average: {calculate_average_bpm(timestamps)}BPM")
+    if write:
+        print(f"{calculate_last_bpm(timestamps)}, average: {calculate_average_bpm(timestamps)}")
+
+def calculate_last_bpm(timestamps):
+    assert type(timestamps) == list, "input should be a list"
+    if len(timestamps) == 1: return "not enough data"
+    return 60.0/(timestamps[-1] - timestamps[-2])
 
 def calculate_average_bpm(timestamps):
     average_delta_time = calculate_average_delta_time(timestamps)
-    if type(average_delta_time) == str:
-        return average_delta_time
-    else:
-        return 60 / average_delta_time
+    is_string = type(average_delta_time) == str
+    return average_delta_time if is_string else 60 / average_delta_time
 
-def calculate_last_bpm(timestamps):
-    if type(timestamps) != list: raise RuntimeError("input should be a list")
-    if len(timestamps) == 1: return "not enough data"
-    return 60/(timestamps[-1] - timestamps[-2])
 
 def calculate_average_delta_time(timestamps):
-    if type(timestamps) != list: raise RuntimeError("input should be a list")
+    assert type(timestamps) == list, "input should be a list"
     if len(timestamps) == 1: return "not enough data"
     all_delta_times = []
     previous_item = None
     for i in timestamps:
-        if previous_item == None:
-            previous_item = i
-        else:
-            all_delta_times.append(i - previous_item)
-            previous_item = i
+        if previous_item != None:   all_delta_times.append(i - previous_item)
+        previous_item = i      
     return calculate_average(all_delta_times)
 
 def calculate_average(sequence):
-    if type(sequence) != list: raise RuntimeError("input should be a list")
-    total = 0
-    for i in sequence:
-        total += i
+    assert type(sequence) == list, "input should be a list"
+    total = sum(sequence)
     return total / len(sequence)
-add_current_time(timestamps)
-keyboard.add_hotkey("space", add_current_time, args=(timestamps,))
-keyboard.wait("alt")
+
+if __name__ == "__main__":
+    main()
